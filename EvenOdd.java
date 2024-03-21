4470b3595a81d12474ef7f9d9afa9d1819e2aad2
Leeminseo <mslee041219@gmail.com>
2024-03-21 13:42:58 +0900

    짜수/홀수 구별 프로그램

---
+++ EvenOdd.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input=new Scanner(System.in);
+		int n;
+		int s; //나누어진 수
+		
+		System.out.print("정수를 입력하시오:");
+		n=input.nextInt();
+		
+		s=n%2;
+		
+		
+		if(s==0)
+			System.out.print("짝수입니다");
+		else
+			System.out.print("0이거나 홀수입니다.");
+				
+	}
+
+}
